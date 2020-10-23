dacaffcb6fbc9aa2fc70feaad936d0e3ae88cb0a
Alessio Manieri <mani.alexander03@gmail.com>
2020-10-23 19:57:17 +0200

    Aggiungere i file di progetto.

---
+++ LibreriaLampadina/Lampadina.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaLampadina
+{
+    public class Lampadina
+    {
+        public const int STATOSPENTA = 0;
+        public const int STATOACCESA = 1;
+        public const int STATOROTTA = -1;
+        private const int CLICK_PRIMA_DI_ROTTURA = 30;
+
+        private int stato = STATOROTTA;
+        private int nClick = 0;
+
+        public Lampadina()
+        { }
+
+
+
+        public int Click()
+        {
+            nClick++;
+            if (nClick >= CLICK_PRIMA_DI_ROTTURA)
+            {
+                stato = STATOROTTA;
+            }
+            else
+            {
+                stato = 1 - stato;
+            }
+            return stato;
+
+
+        }
+
+        public string Descrizione()
+        {
+
+            if (stato == STATOACCESA)
+            {
+                return "La lampadina è accesa";
+            }
+            else if (stato == STATOSPENTA)
+            {
+                return "La lampadina è spenta";
+
+            }
+            else
+                return "La lampadina è rotta";
+
+
+
+        }
+
+    }
+}
---
+++ MainLampadina/Program.cs
@@ -0,0 +1,31 @@
+﻿using LibreriaLampadina;
+using System;
+
+namespace MainLampadina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampadina l = new Lampadina();
+
+            Console.WriteLine("MENU LAMPADINA");
+            Console.WriteLine("Vuoi eseguire un click o sapere lo stato attuale della Lampadina? (click/stato)");
+            string risposta = Console.ReadLine();
+
+            if (risposta == "click")
+            {
+                l.Click();
+                Console.WriteLine("Lo stato della lampadina è stato aggiornato");
+            }
+            if (risposta == "stato")
+            {
+                Console.WriteLine(l.Descrizione());
+            }
+            else
+            {
+                Console.WriteLine("Risposta non valida");
+            }
+        }
+    }
+}
